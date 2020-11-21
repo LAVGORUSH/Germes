dddcf2e27c3410569e60e5c0e46e2d01035a1516
Lebedev Anton <lavgorush@gmail.com>
2020-11-21 16:10:13 +0400

    Add simple rest service

---
+++ germes-service/src/main/java/com/lavgorush/germes/app/rest/service/CityResource.java
@@ -0,0 +1,18 @@
+package com.lavgorush.germes.app.rest.service;
+
+import com.google.common.collect.Lists;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.util.List;
+
+@Path("cities")
+public class CityResource {
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<String> findCities() {
+        return Lists.newArrayList("Odessa", "Kiyv");
+    }
+}
